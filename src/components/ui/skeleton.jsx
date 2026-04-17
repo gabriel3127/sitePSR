22d8ec5649fb3fdfdd2cbdffe79ea2bd4747b21c
gabriel3127 <53674041+gabriel3127@users.noreply.github.com>
2026-04-17 16:17:13 -0300

    Delete src directory

--- src/components/ui/skeleton.jsx
+++
@@ -1,9 +0,0 @@
-﻿import { cn } from "@/lib/utils";
-
-function Skeleton({ className, ...props }: React.HTMLAttributes) {
-  return <div className={cn("animate-pulse rounded-md bg-muted", className)} {...props} />;
-}
-
-export { Skeleton };
-
-
